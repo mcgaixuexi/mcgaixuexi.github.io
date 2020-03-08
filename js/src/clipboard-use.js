dd46a0bb36344140d55bbeaede42ec1c7e36508c
mcgaixuexi <mcgaixuexi@qq.com>
2020-03-08 15:55:55 +0800

    Site updated: 2020-03-08 15:55:51

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,1 @@
-// build time:Sun Mar 08 2020 15:08:24 GMT+0800 (GMT+08:00)
-!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
-//rebuild by neat 
+"use strict";window,document,$(".highlight .code pre").before('<button class="btn-copy" data-clipboard-snippet="">  <i class="fa fa-globe"></i><span>copy</span></button>'),new ClipboardJS(".btn-copy",{target:function(t){return t.nextElementSibling}});
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,1 @@
-// build time:Sun Mar 08 2020 15:08:25 GMT+0800 (GMT+08:00)
-$(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
-//rebuild by neat 
+"use strict";$(document).ready(function(){var o,i=window.location.href.replace(window.location.origin,"");if($(window).on("scroll",function(){clearTimeout(o),o=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+i,{expires:365,path:""})},250)}),void 0!==Cookies.get("scroll-cookie")){var e=Cookies.get("scroll-cookie").split("|");e[1]==i&&$(window).scrollTop(e[0])}});
