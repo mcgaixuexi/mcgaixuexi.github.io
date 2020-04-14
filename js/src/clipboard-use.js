7385e14af47550a7382ab513a11c2354c070c336
mcgaixuexi <mcgaixuexi@qq.com>
2020-04-14 09:18:17 +0800

    Site updated: 2020-04-14 09:18:02

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 10 2020 17:09:17 GMT+0800 (GMT+08:00)
+// build time:Tue Apr 14 2020 09:16:21 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
